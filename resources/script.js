fdfd1383975266b43051bdb73f217c7365f0ab46
snigdho611 <snigdho.howlader@gmail.com>
2021-09-20 06:58:17 +0600

    adding styles and input click handler

--- resources/script.js
+++ resources/script.js
@@ -1,1 +1,6 @@
 console.log('script detected');
+
+const onInputClick = (e) => {
+    console.log(e.id);
+    document.getElementById('input').value = document.getElementById('input').value+e.id;
+}
