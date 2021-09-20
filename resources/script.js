7288549e5ff56d679845664787efb6a9f757d441
snigdho611 <snigdho.howlader@gmail.com>
2021-09-21 01:41:56 +0600

    Added functional operations to create the output

--- resources/script.js
+++ resources/script.js
@@ -6,70 +6,67 @@ let inputBox = document.getElementById('inputBox');
 let outputBox = document.getElementById('outputBox');
 
 const getResult = (input_1, input_2, operation) => {
-    if(operation == "+")
-    {
+    if(operation == "+"){
         return parseInt(input_1)+parseInt(input_2);
     }
-    else if(operation == "-")
-    {
+    else if(operation == "-"){
         return parseInt(input_1)-parseInt(input_2);
     }
-    else if(operation == "x")
-    {
+    else if(operation == "×"){
         return parseInt(input_1)*parseInt(input_2);
     }
-    else if(operation == "/")
-    {
+    else if(operation == "/"){
         return parseInt(input_1)/parseInt(input_2);
     }
 }
 
 const onInputClick = (e) => {
     let first_val, second_val, res_val;
-    if(e.target.id == "add" || e.target.id == "sub" || e.target.id == "mul"  || e.target.id == "div" )
-    {
-        if(document.getElementById('outputBox').value == "")
-        {
-            if(operatorFlag == true)
-            {
+    if(e.target.id == "c"){
+        inputBox.value = inputBox.value.substring(0, inputBox.value.length-1);
+    }
+    else if(e.target.id == "del"){
+        inputBox.value = "";
+        outputBox.value = "";
+    }
+    else if(e.target.id == "eql"){
+        
+    }
+    // console.log(e.target.id)
+    else if(e.target.id == "add" || e.target.id == "sub" || e.target.id == "mul"  || e.target.id == "div" ){
+        if(document.getElementById('outputBox').value == ""){
+            if(operatorFlag == true){
+                let arr = inputBox.value.split(operation);
+                first_val = arr[0];
+                second_val = arr[1];
+                res_val = getResult(first_val, second_val, operation);
+                getResult(first_val, second_val, operation);
+                console.log(getResult(first_val, second_val, operation));
+                outputBox.value = getResult(first_val, second_val, operation);
                 inputBox.value = "";
                 operatorFlag = false;
-                // let arr = inputBox.value.split(operation);
-                // first_val = arr[0];
-                // second_val = arr[1];
-
-                // res_val = getResult(first_val, second_val, operation);
-
-                // console.log(res_val);
-                // document.getElementById('outputBox').value = res_val;
-                // inputBox.value = e.target.id;
-                // operatorFlag = false;
             }
-            else
-            {
+            else{
                 // inputBox.value = inputBox.value + e.target.value;
                 operation = e.target.value;
                 operatorFlag = true;
             }
         }
-        else
-        {
-            
+        else{
+            operation = inputBox.value[0];
+            first_val = outputBox.value;
+            second_val = inputBox.value.substring(1, inputBox.value.length);
+            inputBox.value = "";
+            console.log('result: '+getResult(first_val, second_val, operation));
+            outputBox.value = getResult(first_val, second_val, operation);
         }
-        console.log(e.target.value);
         inputBox.value = inputBox.value + e.target.value;
     }
-    else
-    {
-        //continue
+    else{
         inputBox.value = inputBox.value + e.target.id;
     }
+    // console.log(inputBox.value)
 }
-
-// const onInputChange = (e) => {
-//     console.log('ok')
-// }
-
 
 const updateValue = (e) => {
     console.log(e.target.value[e.target.value.length-1]);
@@ -78,6 +75,7 @@ const updateValue = (e) => {
 document.getElementById('inputBox').addEventListener('input', updateValue);
 
 //Numbers
+document.getElementById('0').addEventListener('click', onInputClick);
 document.getElementById('1').addEventListener('click', onInputClick);
 document.getElementById('2').addEventListener('click', onInputClick);
 document.getElementById('3').addEventListener('click', onInputClick);
@@ -93,3 +91,10 @@ document.getElementById('add').addEventListener('click', onInputClick);
 document.getElementById('sub').addEventListener('click', onInputClick);
 document.getElementById('mul').addEventListener('click', onInputClick);
 document.getElementById('div').addEventListener('click', onInputClick);
+
+//Others
+document.getElementById('del').addEventListener('click', onInputClick);
+document.getElementById('c').addEventListener('click', onInputClick);
+document.getElementById('eql').addEventListener('click', onInputClick);
+document.getElementById('exp').addEventListener('click', onInputClick);
+document.getElementById('pi').addEventListener('click', onInputClick);
